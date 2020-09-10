ab156b1e10d50df8a4027aeb5a58e95e551b1c22
todos <zhangxiushanver@163.com>
2020-09-10 21:36:27 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,152 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!-- <router-link to="/">Home</router-link> |
+    <router-view/> -->
+    <div class="header">
+      <div class="center">
+        <img src="../src/assets/img/logo.png">
+        <div class="right">
+          <a href="#" class="home active">首页</a>
+          <a href="http://47.242.128.7/docs/OEX%E7%99%BD%E7%9A%AE%E4%B9%A61.0.pdf" target="_blank" class="whitebook">白皮书</a>
+         
+          <a href="http://oexchain.io" target="_blank" class="borser">区块浏览器</a>
+          <a class="download" href="#">
+            <img src="../src/assets/img/phone.png"/>
+            APP下载
+          </a>
+        </div>
+      </div>
+      <div class="lanuage">
+        <span class="lanuage_active">CN</span>
+        <span>/</span>
+        <span>EN</span>
+      </div>
     </div>
-    <router-view/>
+    <img src="../src/assets/img/banner.png" class="banner"/>
+
+    <div class="part_one">
+      <div class="part_one_center">
+        <div class="img_box">
+          <img src="../src/assets/img/part_one.png"/>
+        </div>
+        <div class="part_one_text">
+          <div class="text_box">
+              创造性的父子账号功能，可以大大降低普通用户使用区块链产品的门槛，受益区块链发展的红利，做到链上交易小额便捷，大额安全
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="part_two">
+      <div class="part_tow_center">
+        <div class="left_align">
+          <div class="item green">
+            <div class="item_left item_one">
+                <img src="../src/assets/img/item1.png"/>
+                <span>cb—dpos机制</span>
+            </div>
+            <div class="item_right">
+                以投票模型实行投票挖矿奖励机制，并将得票率排名较高的节点出块奖励分配给投票人和超级记账节点，从而甄别出稳定性高的超级节点，并提升整个系统的记账的稳定性和安全性。
+            </div>
+          </div>
+        </div>
+
+        <div class="right_align">
+          <div class="item gray">
+            <div class="item_left item_two">
+                <img src="../src/assets/img/item2.png"/>
+                <span>支持发行</span>
+                <span>原生数字资产</span>
+            </div>
+            <div class="item_right">
+                OEX公链将底层原生组件开放出来给用户使用，用户可以在OEX公链上发行属于自己的“原生资产”。用户在OEX公链上发行资产时可不再编写智能合约。只需要在钱包界面填写资产信息便可做到一键发行。
+            </div>
+          </div>
+        </div>
+
+        <div class="left_align">
+          <div class="item green">
+            <div class="item_left item_three">
+                <img src="../src/assets/img/item3.png"/>
+                <span>一键数字资产发行</span>
+            </div>
+            <div class="item_right">
+               大部分智能合约在合约安全方面都做得并不理想，从而引发资产安全风险，OEX采用了多资产安全保障机制，一旦代币进入私钥地址，合约/代币发起方的合约权限操作该笔资产，从而让用户资产更安全。
+            </div>
+          </div>
+        </div>
+
+
+        <div class="right_align">
+          <div class="item gray" >
+            <div class="item_left item_four">
+                <img src="../src/assets/img/item4.png"/>
+                <span>链上手续费</span>
+                <span>分配机制</span>
+            </div>
+            <div class="item_right">
+                创新的手续费分配机制———合约开发者、记账节点、公链代币持有用户三方能持续分享公链生态发展红利。
+            </div>
+          </div>
+        </div>
+
+        <div class="left_align">
+          <div class="item green" style="margin-bottom:0">
+            <div class="item_left item_five">
+                <img src="../src/assets/img/item5.png"/>
+                <span>链上快照</span>
+            </div>
+            <div class="item_right">
+               运用链上快照技术，所有用户资产和合约数据都可以进行基于时间维度的快照，增加DEFI应用场景的可拓展性，增加搭积木式可组合性。
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="part_three">
+      <div class="part_three_center">
+        <div class="center_left">
+          <div class="text_oex">OEX</div>
+          <div class="text_text">代币分配机制</div>
+          <div class="text_text_account">总量限制10亿枚</div>
+        </div>
+        <img src="../src/assets/img/part_three_right.png" class="center_right"/>
+        
+      </div>
+    </div>
+    
+    <div class="part_four">
+      <div class="text_partner">生态合作伙伴申请中</div>
+      <img src="../src/assets/img/join_us.png" class="join_us"/>
+    </div>
+
+    <div class="footer">
+      <div class="footer_center">
+        <div class="center_left footer_left">
+          <img src="../src/assets/img/footer_left.png"/>
+        </div>
+        <div class="footer_right">
+          <div class="right_top">
+            <a href="#"><img src="../src/assets/img/f_r_1.png"/></a>
+            <a href="#"><img src="../src/assets/img/f_r_2.png"/></a>
+            <a href="#"><img src="../src/assets/img/f_r_3.png"/></a>
+            <a href="#"><img src="../src/assets/img/f_r_4.png"/></a>
+          </div>
+          <p>© 2019 Theme designed by oex.com</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  @import url('../src/assets/styles/index.css');
 </style>
+
+
+
+
+          
+          
